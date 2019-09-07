a1b81529d3e3860aa0f5e3381db4099cc5f5bb29
Karina <karinam6@interia.pl>
2019-09-07 13:56:56 +0200

    Configuration First Controller

---
+++ src/main/java/com/example/CosmeticsInciStore/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.CosmeticsInciStore.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+public class HelloController {
+
+
+    @RequestMapping(value="/", method = RequestMethod.GET) //oblsuguje tylko GET
+    public String showHelloPage( ModelMap model){
+        return "hello";
+    }
+
+
+}
