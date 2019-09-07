c91f8dad4f2de17193242f1fe03d2560f87585a1
Karina <karinam6@interia.pl>
2019-09-07 11:56:35 +0200

    Init repository

---
+++ src/main/java/com/example/CosmeticsInciStore/CosmeticsInciStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.CosmeticsInciStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmeticsInciStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmeticsInciStoreApplication.class, args);
+	}
+
+}
