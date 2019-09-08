883f1a6468301ef02c30b7c862e063c2bb1f57b2
Karina <karinam6@interia.pl>
2019-09-08 14:23:09 +0200

    sql

--- src/main/java/com/example/CosmeticsInciStore/CosmeticsInciStoreApplication.java
+++ src/main/java/com/example/CosmeticsInciStore/CosmeticsInciStoreApplication.java
@@ -1,13 +1,17 @@
 package com.example.CosmeticsInciStore;
 
+import com.example.CosmeticsInciStore.entity.User;
+import com.example.CosmeticsInciStore.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class CosmeticsInciStoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CosmeticsInciStoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CosmeticsInciStoreApplication.class, args);
+
+    }
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into user values(1,'email@email.com','name','pass','surname'); --nie mozna tak dodac uzytkownika? ie przechlodzi przez szyfrowanie?
+insert into user values(1,'email@email.com','name','{bcrypt}$2y$12$TnJ3.QJwxi1ldMXtxnsSsOQWOu8HiqAnSd7jz6vEaSCYvXqI4Fz1G','surname'); --nie mozna tak dodac uzytkownika? ie przechlodzi przez szyfrowanie?
 insert into role values(1,'ADMIN');
 insert into user_role values(1,1);
 
