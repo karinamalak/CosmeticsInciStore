3e2e5ec4852c76b624575cc79518275b818a6a30
gosia <malgorzatahosni@gmail.com>
2019-09-14 10:30:34 +0200

    /hello 1

--- src/main/java/com/example/CosmeticsInciStore/controller/HelloController.java
+++ src/main/java/com/example/CosmeticsInciStore/controller/HelloController.java
@@ -1,15 +1,10 @@
 package com.example.CosmeticsInciStore.controller;
 
 
-import com.example.CosmeticsInciStore.entity.Product;
-import com.example.CosmeticsInciStore.service.ProductService;
 import org.springframework.stereotype.Controller;
 
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 
 @Controller
