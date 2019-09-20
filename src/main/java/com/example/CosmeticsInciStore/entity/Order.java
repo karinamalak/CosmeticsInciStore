4fa13881c5214467dff1b8003fd0790d25f5a323
Karina <karinam6@interia.pl>
2019-09-20 15:05:37 +0200

    commit1

--- src/main/java/com/example/CosmeticsInciStore/entity/Order.java
+++ src/main/java/com/example/CosmeticsInciStore/entity/Order.java
@@ -4,6 +4,7 @@ package com.example.CosmeticsInciStore.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Data
 @Builder
@@ -25,9 +26,8 @@ public class Order {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @OneToMany
-    @JoinColumn(name = "product_id")
-    private Product product;
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Product> products;
 
     @Column (name = "sum")
     private Long sum;
